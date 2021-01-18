c3bfbc6279eee600db2cbe707bca0e2ec7953617
kylelainez <kyleadrianlainez@gmail.com>
2021-01-17 19:46:12 -0800

    Chat Component

--- src/components/ChatComponent/ChatComponent.jsx
+++ src/components/ChatComponent/ChatComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ChatComponent.css';
 import { Grid } from 'semantic-ui-react';
+import Image from '../../images/insert_photo-24px.svg';
 
 export default function () {
 	return (
@@ -8,11 +9,20 @@ export default function () {
 			id="Chat-Component"
 			style={{ height: '100vh', padding: 0, margin: 0 }}>
 			<Grid.Row id="TopBar">top bar</Grid.Row>
-			<Grid.Row id="MessageField">
-				Message Field
-				<br /> test
-			</Grid.Row>
-			<Grid.Row id="ChatField">Chat Field</Grid.Row>
+			<Grid.Row id="MessageField">Message Field</Grid.Row>
+			<Grid divided="vertically" id="ChatField">
+				<Grid.Row columns={3}>
+					<Grid.Column>
+						<img src={Image} />
+					</Grid.Column>
+					<Grid.Column>
+						<form>
+							<input type="text" name="message" />
+						</form>
+					</Grid.Column>
+					<Grid.Column>test</Grid.Column>
+				</Grid.Row>
+			</Grid>
 		</Grid.Column>
 	);
 }
